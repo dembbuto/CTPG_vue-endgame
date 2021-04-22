e1b3b461eddf2600ecb928d53eb1c7d88cedc992
Bangmond <dembbuto@naver.com>
2021-04-22 18:12:00 +0900

    학습 노트 데이터 삭제

--- src/components/posts/PostAddForm.vue
+++ src/components/posts/PostAddForm.vue
@@ -50,6 +50,7 @@ export default {
 					contents: this.contents,
 				});
 				console.log(response);
+				this.$router.push('/main');
 			} catch (error) {
 				console.log(error.response.data.message);
 				this.logMessage = error.response.data.message;
