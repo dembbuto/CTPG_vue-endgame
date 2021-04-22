20bdc535f360f0fc118e4d1c22397003b84cd55e
Bangmond <dembbuto@naver.com>
2021-04-22 12:05:20 +0900

    로그인 상태 관리와 스토어

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,25 +1,35 @@
 <template>
-	<form @submit.prevent="submitForm">
-		<div>
-			<label for="username">id: </label>
-			<input id="username" type="text" v-model="username" />
+	<div class="contents">
+		<div class="form-wrapper form-wrapper-sm">
+			<form @submit.prevent="submitForm" class="form">
+				<div>
+					<label for="username">id: </label>
+					<input id="username" type="text" v-model="username" />
+					<p class="validation-text">
+						<span class="warning" v-if="!isUsernameValid && username">
+							Please enter an email address
+						</span>
+					</p>
+				</div>
+				<div>
+					<label for="password">pw: </label>
+					<input id="password" type="text" v-model="password" />
+				</div>
+				<div>
+					<label for="nickname">nickname: </label>
+					<input id="nickname" type="text" v-model="nickname" />
+				</div>
+				<button
+					:disabled="!isUsernameValid || !password || !nickname"
+					type="submit"
+					class="btn"
+				>
+					회원 가입
+				</button>
+			</form>
+			<p class="log">{{ logMessage }}</p>
 		</div>
-		<div>
-			<label for="password">pw: </label>
-			<input id="password" type="text" v-model="password" />
-		</div>
-		<div>
-			<label for="nickname">nickname: </label>
-			<input id="nickname" type="text" v-model="nickname" />
-		</div>
-		<button
-			:disabled="!isUsernameValid || !password || !nickname"
-			type="submit"
-		>
-			회원 가입
-		</button>
-		<p>{{ logMessage }}</p>
-	</form>
+	</div>
 </template>
 
 <script>
